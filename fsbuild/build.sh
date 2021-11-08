97eee7b31e384983f0776d58e3ddcc8beaebea11
Frode Solheim <frode@solheim.dev>
2021-11-08 21:46:26 +0100

    fsbuild

---
+++ fsbuild/build.sh
@@ -0,0 +1,42 @@
+. ./PACKAGE.FS
+. fsbuild/system.sh
+
+make
+
+BUILDDIR=fsbuild/_build
+
+rm -Rf $BUILDDIR/pyinstaller
+if [ "$SYSTEM_OS" = "Windows" ]; then
+pyinstaller \
+	--specpath pyinstaller \
+	--distpath $BUILDDIR/pyinstaller \
+	--log-level DEBUG \
+	--windowed \
+	$PACKAGE_NAME
+BINDIR=fsbuild/_build/pyinstaller/$PACKAGE_NAME
+elif [ "$SYSTEM_OS" = "macOS" ]; then
+pyinstaller \
+	--specpath pyinstaller \
+	--distpath $BUILDDIR/pyinstaller \
+	--log-level DEBUG \
+	--windowed \
+	--osx-bundle-identifier no.fengestad.fs-uae-launcher \
+	$PACKAGE_NAME
+BINDIR=fsbuild/_build/pyinstaller/$PACKAGE_NAME.app/Contents/MacOS
+else
+pyinstaller \
+	--specpath pyinstaller \
+	--distpath $BUILDDIR/pyinstaller \
+	--log-level DEBUG \
+	$PACKAGE_NAME
+BINDIR=fsbuild/_build/pyinstaller/$PACKAGE_NAME
+fi
+
+# These do not work with macOS notarization, but might as well remove for all
+# platforms.
+rm -Rf $BINDIR/PyQt5/Qt/translations
+rm -Rf $BINDIR/PyQt5/Qt/qml
+
+# In case the Qt dir is Qt5...
+rm -Rf $BINDIR/PyQt5/Qt5/translations
+rm -Rf $BINDIR/PyQt5/Qt5/qml

e94559cb64e87246f7c21cb7cbc83275aaa746a8
Frode Solheim <frode@solheim.dev>
2021-11-08 21:25:57 +0100

    Updated config.py to use local packages

--- config.py
+++ config.py
@@ -6,17 +6,17 @@ version = "9.8.7dummy"
 author = "Frode Solheim"
 author_email = "frode@fs-uae.net"
 package_map = {
-    "arcade": "../fs-uae-launcher",
-    "fsbc": "../fs-uae-launcher",
-    "fsboot": "../fs-uae-launcher",
-    "fsgs": "../fs-uae-launcher",
-    "fspy": "../fs-uae-launcher",
-    "fstd": "../fs-uae-launcher",
-    "fsui": "../fs-uae-launcher",
-    "launcher": "../fs-uae-launcher",
-    "OpenGL": "../fs-uae-launcher",
-    "oyoyo": "../fs-uae-launcher",
-    "workspace": "../fs-uae-launcher",
+    "arcade": ".",
+    "fsbc": ".",
+    "fsboot": ".",
+    "fsgs": ".",
+    "fspy": ".",
+    "fstd": ".",
+    "fsui": ".",
+    "launcher": ".",
+    "OpenGL": ".",
+    "oyoyo": ".",
+    "workspace": ".",
 }
 packages = sorted(package_map.keys())
 scripts = ["fs-uae-arcade"]
